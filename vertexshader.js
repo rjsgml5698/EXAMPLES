0e8707840a9eddecbff1fc9dd0542c33875d7620
rjsgml5698 <40594949+rjsgml5698@users.noreply.github.com>
2019-05-02 14:24:19 +0900

    Add files via upload

---
+++ vertexshader.js
@@ -0,0 +1,10 @@
+export const vertexshader = () =>`
+  // Amount of transparency for line, calculated in Animate
+  attribute float scale;
+
+  void main() {
+    vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );
+    gl_PointSize = scale * ( 300.0 / - mvPosition.z );
+    gl_Position = projectionMatrix * mvPosition;
+  }
+`;
