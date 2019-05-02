96624900eed858af47b108cbebad5d1b2e8bf11c
rjsgml5698 <40594949+rjsgml5698@users.noreply.github.com>
2019-05-02 14:34:07 +0900

    Update fragmentshader.js

--- fragmentshader.js
+++ fragmentshader.js
@@ -1,8 +1,8 @@
-export const fragmentshader = () =>`
+export const fragmentshader = () =>
   // Amount of transparency for line, calculated in Animate
   uniform vec3 color;
   void main() {
     if ( length( gl_PointCoord - vec2( 0.5, 0.5 ) ) > 0.475 ) discard;
     gl_FragColor = vec4( color, 1.0 );
   }
-`;
+;
