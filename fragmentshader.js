4be91ddd1c975bab7b89ad41a04ba5fbbc777424
rjsgml5698 <40594949+rjsgml5698@users.noreply.github.com>
2019-05-02 14:41:07 +0900

    Update fragmentshader.js

--- fragmentshader.js
+++ fragmentshader.js
@@ -1,6 +1,10 @@
+export const vertexshader = () =>`
+  // Amount of transparency for line, calculated in Animate
+  attribute float scale;
 
   void main() {
-    if ( length( gl_PointCoord - vec2( 0.5, 0.5 ) ) > 0.475 ) discard;
-    gl_FragColor = vec4( color, 1.0 );
+    vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );
+    gl_PointSize = scale * ( 300.0 / - mvPosition.z );
+    gl_Position = projectionMatrix * mvPosition;
   }
-
+`;
